6c0118ac10926166c3636faa81b0442bd63da1e7
matheusrodrigues1 <matech00@outlook.com>
2024-08-19 14:40:02 -0300

    pricing section

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -8,14 +8,14 @@ export const ProductShowcase = () => {
     <>
       <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
         <div className="container">
-          <div className="max-w-[540px] mx-auto">
+          <div className="section-heading">
             <div className="flex justify-center">
               <div className="tag">Boost your producttivy</div>
             </div>
-            <h2 className="text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">
+            <h2 className="section-title mt-5">
               A more effective way to track progress
             </h2>
-            <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5">
+            <p className="section-description mt-5">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Temporibus perferendis debitis cupiditate, magnam commodi tempore
               similique magni reprehenderit
