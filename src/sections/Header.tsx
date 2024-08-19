d574252461c75d4006f9ea74a2b36092acf2f9d1
matheusrodrigues1 <matech00@outlook.com>
2024-08-19 11:50:07 -0300

    hero section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export const Header = () => {
   return (
-    <header className="sticky top-0">
+    <header className="sticky top-0 backdrop-blur-sm z-20">
       <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
         <p className="text-white/60 hidden md:block">
           Streamline your workflow and boost your productivy
@@ -26,7 +26,7 @@ export const Header = () => {
               <a href="">Customers</a>
               <a href="">Updates</a>
               <a href="">Help</a>
-              <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex align-items justify-cente tracking-tight">
+              <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex align-items justify-center tracking-tight">
                 Get fro free
               </button>
             </nav>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,56 @@
+import ArrowIcon from "@/assets/arrow-right.svg";
+import cogImage from "@/assets/cog.png";
+import cylinderImage from "@/assets/cylinder.png";
+import noodleImage from "@/assets/noodle.png";
+import Image from "next/image";
+
 export const Hero = () => {
-  return null;
+  return (
+    <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip">
+      <div className="container">
+        <div className="md:flex items-center">
+          <div className="md:w-[478px]">
+            <div className="text-sm inline-flex border border-[#222]/10 py-1 px-3 rounded-lg tracking-tight">
+              Version 2.0 is here
+            </div>
+            <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
+              Pathway to productivy
+            </h1>
+            <p className="text-xl text-[#010D3E] tracking-tight mt-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
+              debitis dolorem magnam suscipit aperiam nesciunt labore ducimus
+              quae veniam pariatur perspiciatis, consequuntur sed alias.
+            </p>
+            <div className="flex gap-1 items-center mt-[30px]">
+              <button className="btn btn-primary">Get for free</button>
+              <button className="btn btn-text gap-1">
+                <span>Learn more</span>
+                <ArrowIcon className="h-5 w-5" />
+              </button>
+            </div>
+          </div>
+          <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
+            <Image
+              src={cogImage}
+              alt="cog image"
+              className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0"
+            />
+            <Image
+              src={cylinderImage}
+              width={220}
+              height={220}
+              alt="cylinder"
+              className="hidden md:block -top-8 -left-32 md:absolute"
+            />
+            <Image
+              src={noodleImage}
+              width={220}
+              alt="cylinder"
+              className="hidden lg:block absolute top-[524px] left-[448px] rotate-[30deg]"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
