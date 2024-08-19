2df1e12b7c1006df5895684042caa6b6c2c354d2
matheusrodrigues1 <matech00@outlook.com>
2024-08-19 13:16:43 -0300

    product show case section

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,3 +1,44 @@
+import productImage from "@/assets/product-image.png";
+import pyramidImage from "@/assets/pyramid.png";
+import tubeImage from "@/assets/tube.png";
+import Image from "next/image";
+
 export const ProductShowcase = () => {
-  return null;
+  return (
+    <>
+      <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
+        <div className="container">
+          <div className="max-w-[540px] mx-auto">
+            <div className="flex justify-center">
+              <div className="tag">Boost your producttivy</div>
+            </div>
+            <h2 className="text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">
+              A more effective way to track progress
+            </h2>
+            <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Temporibus perferendis debitis cupiditate, magnam commodi tempore
+              similique magni reprehenderit
+            </p>
+          </div>
+          <div className="relative">
+            <Image src={productImage} alt="product" className="mt-10" />
+            <Image
+              src={pyramidImage}
+              alt="pyramid"
+              height={262}
+              width={262}
+              className="hidden md:block absolute -right-36 -top-32"
+            />
+            <Image
+              src={tubeImage}
+              alt="tube"
+              height={248}
+              className="hidden md:block absolute bottom-24 -left-36"
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
